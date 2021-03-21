50e28c765c003ce1ee6dd4e8ab5adca9312b0b0f
codelint <unotseeme@foxmail.com>
2021-03-21 17:52:36 +0800

    Initial Commit

---
+++ going/goingApp.swift
@@ -0,0 +1,20 @@
+//
+//  goingApp.swift
+//  going
+//
+//  Created by gzhang on 2021/3/21.
+//
+
+import SwiftUI
+
+@main
+struct goingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
